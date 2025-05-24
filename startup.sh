27669ab66680ecc5c9bcb9924504942affa10a9c
bck0593 <bck88884@gmail.com>
2025-05-25 00:51:50 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 依存パッケージをインストール
+# pip で必要なものをグローバルインストール
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # アプリケーションを起動
-exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app
 
